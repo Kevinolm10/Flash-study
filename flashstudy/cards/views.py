25198a32f08928b973fedbb6091a64495c58246c
Kevinolm10 <kevinolm10@gmail.com>
2025-07-22 15:59:07 +0200

    Add pagination for the card column

--- flashstudy/cards/views.py
+++ flashstudy/cards/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Card, Deck
+from django.core.paginator import Paginator
 
 def homePage(request):
     # Get all cards or cards from a specific deck
-    cards = Card.objects.all()  # or filter by deck if needed
+    cards = Card.objects.all()
+    paginator = Paginator(cards, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     
     context = {
-        'cards': cards,
+        'page_obj': page_obj,
     }
     return render(request, 'cards.html', context)
